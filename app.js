f3dfabd602bb2ff7e0623b4899b1f9f1f0eac92f
Samuel Liziard <sliziard@icloud.com>
2023-04-05 20:50:34 +0200

    ajt midleware register check si le nom est connu

--- app.js
+++ app.js
@@ -6,13 +6,11 @@ const app = express();
 
 app.use(express.json());  //TODO intercepts all .json request and puts in req.body
 
+//* connect to mongodb
 const uri = 'mongodb+srv://Manager:fK0NfcW7IcKvT53w@cluster0.46xga.mongodb.net/?retryWrites=true&w=majority';
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
-
-user.find()
-    .then(users => console.log(users))
 
 app.use('', (req, res, next) => {
     console.log('requete reçu');
@@ -21,9 +19,22 @@ app.use('', (req, res, next) => {
 });
 
 //* register
-
- app.post('/register', (req, res, next) => {    //TODO request register
-    
+ app.post('/register', (req, res, next) => {    //TODO check if the username don't already exist
+ let current_user = req.body.username; //? a modifier quand j'aurais trouvé l'accès au nom d'utilisateur
+ user.find().select({ username: 1 })
+  .then(usersname => {
+    let know_user = false;
+    usersname.forEach(user => {
+      if (user.username === current_user) {
+        know_user = true;
+      }
+    })
+    if (know_user) {
+      res.status(400).json({ error: 'Username already exists' });
+    } else {
+      res.status(200).json({ message: 'Valid username' });
+    }
+  });
  });
 
 module.exports = app;
