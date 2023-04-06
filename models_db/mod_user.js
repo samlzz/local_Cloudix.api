c88f680211b18bcffa91ded9975c2ca9fd06527c
Samuel Liziard <sliziard@icloud.com>
2023-04-06 17:21:47 +0200

    ajout de data_name (le pseudo en minuscule)

--- models_db/mod_user.js
+++ models_db/mod_user.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const user_schema = mongoose.Schema({
   cookie: { type: String, required: true },
   username: { type: String, required: true },
+  data_name: { type: String, required: true},
   password: { type: String, required: true },
 });
 
