36bd921b1be45513539dc47ccb927d95406d931b
Samuel Liziard <sliziard@icloud.com>
2023-04-06 17:22:53 +0200

    ajout midleware check si username deja utilisé

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const user = require('./models_db/mod_user');
+const User = require('./models_db/mod_user');
 
 const app = express();
 
@@ -12,25 +12,36 @@ mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
-app.use('', (req, res, next) => {
+app.use('', (req, res, next) => { //! midleware de test à supr plus tard 
     console.log('requete reçu');
     res.status(200).json('test');
     next();
 });
 
+/*
+? ajoute l'utilisateurs du fichier "utilitest" a la base de données
+const utilitest = require('./test/utilitest');
+app.use('', (req, res, next) => {
+  const utili = new User({
+    ...utilitest
+  });
+  utili.save()
+  console.log('enregistré')
+});
+*/
+
 //* register
- app.post('/register', (req, res, next) => {    //TODO check if the username don't already exist
- let current_user = req.body.username; //? a modifier quand j'aurais trouvé l'accès au nom d'utilisateur
- current_user = current_user.toLowerCase();
- user.find({ $text: { $search: username }}).select(current_user)
+app.use('/register', (req, res, next) => {   //TODO check if the username don't already exist
+  let current_user = req.body.user.username; //? a modifier quand j'aurais trouvé l'accès au nom d'utilisateur
+  current_user = current_user.toLowerCase();
+  User.findOne({ data_name: current_user }).select({data_name: 1})
   .then(usersname => {
-    usersname = usersname.toLowerCase();
-    if (usersname === current_user) {
+    if (usersname?.data_name === current_user) {
       res.status(400).json({ error: 'Username already exists' });
     } else {
       res.status(200).json({ message: 'Valid username' });
     }
   });
- });
+});
 
 module.exports = app;
