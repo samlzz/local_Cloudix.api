d05c8487ce486274eb2b1b2577b3be4f01c37392
Samuel Liziard <sliziard@icloud.com>
2023-04-07 20:00:27 +0200

    ajout d'une vérification du mot de passe

--- app.js
+++ app.js
@@ -46,12 +46,36 @@ app.post('/register', (req, res, next) => {   //TODO check if the username don't
 });
 
 //* login
+// ! ! ! a tester ! ! ! //
 app.post('/login', (req, res, next) => { //TODO check if the user who try to login is existing
   let user_who_log = req.body.User.username;  //? a modifier quand j'aurais trouvé l'accès au nom d'utilisateur
+  let mdp_who_log = req.body.User.password;  //? ^^^ idem ^^^
+  let err_t = false;
   user_who_log = user_who_log.toLowerCase();
   User.findOne({ data_name: user_who_log })
-  .then(usersname_l => { res.status(200).json({ message: 'User find', user: usersname_l}) })
-  .catch(error => res.status(400).json({error, message: 'User don t find'}));
+  .then(usersname_l => { 
+    //TODO check if the user are found great
+    if(usersname_l?.data_name === user_who_log) {
+      let mess = 'User find';
+      //TODO check if the password matches
+      if( usersname_l.password === mdp_who_log) {
+        mess += 'and password is correct';
+      } else {
+        err_t = true;
+        mess += 'but wrong password';
+      }
+    } else {
+      err_t = true;
+      let mess = 'User dont find';
+    }
+    //TODO check if it have an error:
+    if (err_t) { 
+      res.status(400).json({ error: mess});
+    } else {  
+      res.status(200).json({ message: mess, user: usersname_l });
+    }
+  })
+  .catch(error => res.status(400).json({ error }));
 });
 
 module.exports = app;
