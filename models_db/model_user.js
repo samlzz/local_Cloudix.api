16bf1f9f8e3e8a83a3d4a20c4948878677f20d15
Samuel Liziard <sliziard@icloud.com>
2023-04-13 17:02:53 +0200

    ajout liste de fichier au model et a l'user test

--- models_db/model_user.js
+++ models_db/model_user.js
@@ -2,9 +2,10 @@ const mongoose = require('mongoose');
 
 const user_schema = mongoose.Schema({
   cookie: { type: String, required: true },
-  username: { type: String, required: true },
   data_name: { type: String, required: true},
+  username: { type: String, required: true },
   password: { type: String, required: true },
+  folder: { type: Array, required: true },
 });
 
 module.exports = mongoose.model('user', user_schema);
