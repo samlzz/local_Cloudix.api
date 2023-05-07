39d518a52074af5b1eef7930222628a1a876b1d1
Samuel Liziard <sliziard@icloud.com>
2023-05-07 12:09:46 +0200

    ajout delete, rename et change categorie

--- server/controllers/files.js
+++ server/controllers/files.js
@@ -8,6 +8,7 @@ const User = require('../models_db/model_user');
 const init_mul = require('./config_multer');
 
 
+//* UPLOAD ONE FILE
 exports.add_one_file_to_user = (req, res) => { 
     let user_name = req.body.username.toLowerCase();  //TODO: suprr .toL... si frontend envoie en min
     if(!path_uploads){
@@ -31,15 +32,17 @@ exports.add_one_file_to_user = (req, res) => {
         return res.status(500).json({ message: 'User not found' });
       };
       user.folder.push({ 
-        path: req.file.path, 
+        path: path.join(path_uploads, req.file.originalname), 
         categorie: req.body.categorie 
       });
-      user.save();
-      res.status(200).json({message: "File uploaded successfully."});
+      user.save()
+      .then(() => res.status(201).json({ message: 'File uploaded successfully' }))
+      .catch(error => res.status(500).json({ error }));
     })
     .catch(error => res.status(500).json({ error }));
 };
 
+//* UPLOAD SOME FILES
 exports.add_somme_files_to_user = (req, res) => {
   let user_name = req.body.username.toLowerCase();  //TODO: suprr .toL... si frontend envoie en min
   if(!path_uploads){
@@ -64,7 +67,7 @@ exports.add_somme_files_to_user = (req, res) => {
     };
     for(let i = 0; i<req.file.length; i++){
       user.folder.push({ 
-        path: req.file.path, 
+        path:path.join(path_uploads, req.file[i].originalname), 
         categorie: req.body.categorie 
       });
     };
@@ -75,6 +78,7 @@ exports.add_somme_files_to_user = (req, res) => {
   .catch(error => res.status(500).json({ error }));
 };
 
+//* RETURN FILE OF A USER
 exports.send_file_of_user = (req, res) => {
   let name_user = req.body.username.toLowerCase();  //TODO: suprr .toL... si frontend envoie
   let files_list = [];
@@ -100,3 +104,76 @@ exports.send_file_of_user = (req, res) => {
   })
   .catch(error => res.status(500).json({ error }));
 };
+
+//* DELETE
+exports.delete_a_file = (req, res) => {
+  User.findOne({ data_name: req.body.data_name })
+  .then(user => {
+    if (!user){
+      return res.status(500).json({ message: 'User not found' });
+    };
+    let file_to_del = user.folder.find(file => file.path.includes(req.body.filename));
+    let his_index = user.folder.findIndex(file_to_del);
+    if (!file_to_del || his_index === -1){
+      return res.status(404).json({ message: 'File to delete not found' });
+    };
+    fs.unlink(file_to_del, (err) => {
+      if (err) {
+        return res.status(500).json(err);
+      };
+      user.folder.splice(his_index, 1);
+      user.save()
+      .then(() => res.status(201).json({ message: 'File removed successfully' }))
+      .catch(error => res.status(400).json({ error }));
+    });
+  })
+  .catch(error => res.status(500).json({ error }));
+};
+
+//* RENAME
+exports.rename_a_file = (req, res) => {
+  User.findOne({ data_name: req.body.data_name })
+  .then(user => {
+    if (!user){
+      return res.status(500).json({ message: 'User not found' });
+    };
+    let file_to_rnm = user.folder.find(file => file.path.includes(req.body.filename));
+    let index_to_rnm = user.folder.findIndex(file_to_rnm);
+    if (!file_to_rnm || index_to_rnm === -1){
+      return res.status(404).json({ message: 'File to rename not found' });
+    };
+    let new_filepath = path.join(__dirname, 'data', user.data_name, req.body.new_filename);
+    fs.rename(file_to_rnm, new_filepath, (err) => {
+      if(err){
+        return res.status(500).json(err);
+      };
+      user.folder[index_to_rnm].path = new_filepath;
+      user.save()
+      .then(() => res.status(201).json({ message: 'File renamed successfully' }))
+      .catch(error => res.status(400).json({ error }));
+    });
+  })
+  .catch(error => res.status(500).json({ error }));
+};
+
+//* PUBLIC --> PRIVE ET INVERSE
+exports.change_categorie = (req, res) => {
+  User.findOne({ data_name: req.body.data_name })
+  .then(user => {
+    let catt_index = user.folder.findIndex(req.body.filename);
+    if(catt_index === -1){
+      return res.status(404).json({ message: 'File to change categorie not found' });
+    };
+    if(req.body.categorie === 'private') {
+      user.folder[catt_index].categorie = 'public';
+    } else if(req.body.categorie === 'public') {
+      user.folder[catt_index].categorie = 'private';
+    } else {
+      return res.status(400).json({ message: 'Invalid categorie are send' });
+    };
+    user.save()
+      .then(() => res.status(201).json({ message: 'Categorie has been changed successfully' }))
+      .catch(error => res.status(400).json({ error }));
+  })
+  .catch(error => res.status(500).json({ error }));
+}; 
--- server/routes.js
+++ server/routes.js
@@ -12,5 +12,8 @@ router.post('/login', ctrl_user.check_id_exist_and_passwrd_valid);
 router.post('/oneupload', ctrl_file.add_one_file_to_user);
 router.post('/multiupload', ctrl_file.add_somme_files_to_user);
 router.get('/fileofuser', ctrl_file.send_file_of_user);
+router.delete('/delfile', ctrl_file.delete_a_file);
+router.post('/rename', ctrl_file.rename_a_file);
+router.post('/changecategorie', ctrl_file.change_categorie);
 
 module.exports = router;
