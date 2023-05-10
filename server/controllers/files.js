99937fbf00c53ed8741afa5df2c97f0f8a7188d5
Samuel Liziard <sliziard@icloud.com>
2023-05-10 07:45:46 +0200

    init multer et gestion de l'upload

--- server/controllers/files.js
+++ server/controllers/files.js
@@ -5,45 +5,38 @@ const fs = require('fs');
 const path = require('path');
 
 const User = require('../models_db/model_user');
-const init_mul = require('./config_multer');
+const Public_files = require('../models_db/model_public_files');
 
 
-//* UPLOAD ONE FILE
-exports.add_one_file_to_user = (req, res) => { 
-    let user_name = req.body.username.toLowerCase();  //TODO: suprr .toL... si frontend envoie en min
-    if(!path_uploads){
-      var path_uploads = path.join(__dirname, 'data', user_name);
-    } else {
-      path_uploads = path.join(__dirname, 'data', user_name);
+//* UPLOAD ONE PRIVATE FILE
+exports.add_one_file_to_user = (req, res) => {
+  if(JSON.parse(req.body.categorie) === 'public'){
+    return res.status(400).json({ message: "Request for a private file and it is a public file" });
+  };
+  let user_name = JSON.parse(req.body.data_name);
+  path_folder = path.join(__dirname, '..', 'data', user_name);
+  //? -->
+  User.findOne({ data_name: user_name})
+  .then(user => {
+    if (!user){
+      return res.status(500).json({ message: 'User not found' });
     };
-    //? -->
-    if(!fs.existsSync(path_uploads)) {  //? check if folder doesn't exist
-        fs.mkdirSync(path_uploads);
-    };
-    const upload = init_mul.single('upload-file');
-    upload(req, res, err => {
-        if (err) {
-            return res.status(500).json({ err });
-        };
+    user.folder.push({ 
+      path: path.join(path_folder, req.file.originalname), 
+      //path: path.join(path_folder, 'begin.txt')
     });
-    User.findOne({ data_name: user_name})
-    .then(user => {
-      if (!user){
-        return res.status(500).json({ message: 'User not found' });
-      };
-      user.folder.push({ 
-        path: path.join(path_uploads, req.file.originalname), 
-        categorie: req.body.categorie 
-      });
-      user.save()
-      .then(() => res.status(201).json({ message: 'File uploaded successfully' }))
-      .catch(error => res.status(500).json({ error }));
-    })
+    user.save()
+    .then(() => res.status(201).json({ message: 'File uploaded successfully' }))
     .catch(error => res.status(500).json({ error }));
+  })
+  .catch(error => res.status(500).json({ error }));
 };
 
-//* UPLOAD SOME FILES
+//* UPLOAD SOME PRIVATE FILES
 exports.add_somme_files_to_user = (req, res) => {
+  if('public' in req.body.categorie){
+    return res.status(400).json({ message: "Request for privates files and it is a publics files" });
+  };
   let user_name = req.body.username.toLowerCase();  //TODO: suprr .toL... si frontend envoie en min
   if(!path_uploads){
     var path_uploads = path.join(__dirname, 'data', user_name);
@@ -51,11 +44,11 @@ exports.add_somme_files_to_user = (req, res) => {
     path_uploads = path.join(__dirname, 'data', user_name);
   };
   //? -->
-  if(!fs.existsSync(path_uploads)) {  //? check if folder doesn't exist
+  if(!fs.existsSync(path_uploads)) {  //? check if folder doesn't exist and create it
       fs.mkdirSync(path_uploads);
   };
-  const upload = init_mul.array('upload-file', 12);
-  upload(req, res, err => {
+  var Upload = init_mul.array('upload-file', 12);
+  Upload(req, res, err => {
       if (err) {
           return res.status(500).json({ err });
       };
@@ -72,13 +65,63 @@ exports.add_somme_files_to_user = (req, res) => {
       });
     };
     user.save()
-    .then(() => res.status(201).json({ message: 'File uploaded successfully' }))
+    .then(() => res.status(201).json({ message: 'Files uploaded successfully' }))
     .catch(error => res.status(400).json({ error }));
   })
   .catch(error => res.status(500).json({ error }));
 };
 
-//* RETURN FILE OF A USER
+//* UPLOAD PUBLIC FILE
+exports.add_file_to_public = (req, res) => {
+  if('private' in req.body.categorie){
+    return res.status(400).json({ message: "Request for a public file and it is a private file" });
+  };
+  if(!path_uploads){
+    var path_uploads = path.join(__dirname, 'data', 'public');
+  } else {
+    path_uploads = path.join(__dirname, 'data', 'public');
+  };
+  if(req.file.length = 1){
+    var Upload = init_mul.single('upload-file');
+    Upload(req, res, err => {
+      if (err) {
+        return res.status(500).json({ err });
+      };
+    });
+    let pu_files = new Public_files({
+      path: path.join(path_uploads, req.file.originalname),
+      name: req.file.originalname,
+    });
+    pu_files.save()
+    .then(() => res.status(201).json({ message: 'File uploaded successfully' }))
+    .catch(error => res.status(400).json({ error }));
+  } else {
+    var Upload = init_mul.array('upload-file', 12);
+    Upload(req, res, err => {
+      if (err) {
+        return res.status(500).json({ err });
+      };
+    });
+    let count = 0
+    let list_error = []
+    for(let i = 0; i<req.file.length; i++){
+      let pu_files = new Public_files({
+        path: path.join(path_uploads, req.file[i].originalname),
+        name: req.file[i].originalname,
+      });
+      pu_files.save()
+      .then(count = count+1)
+      .catch(error => list_error.push(error));
+    };
+    if(count === req.file.length){
+      res.status(201).json({ message: 'All files have beeing uploaded' });
+    } else {
+      res.status(400).json(list_error);
+    }
+  };
+};
+
+//* RETURN FILES OF A USER
 exports.send_file_of_user = (req, res) => {
   let name_user = req.body.username.toLowerCase();  //TODO: suprr .toL... si frontend envoie
   let files_list = [];
@@ -97,7 +140,7 @@ exports.send_file_of_user = (req, res) => {
         categorie: class_fill,
         name: na,
         extension: ext,
-        size: stats.size, //? size in bytes
+        size: stats.size //? size in bytes
       });
     };
     res.status(200).json(files_list);
@@ -156,7 +199,7 @@ exports.rename_a_file = (req, res) => {
   .catch(error => res.status(500).json({ error }));
 };
 
-//* PUBLIC --> PRIVE ET INVERSE
+//* PUBLIC --> PRIVE AND REVERSE
 exports.change_categorie = (req, res) => {
   User.findOne({ data_name: req.body.data_name })
   .then(user => {
@@ -165,9 +208,9 @@ exports.change_categorie = (req, res) => {
       return res.status(404).json({ message: 'File to change categorie not found' });
     };
     if(req.body.categorie === 'private') {
-      user.folder[catt_index].categorie = 'public';
-    } else if(req.body.categorie === 'public') {
       user.folder[catt_index].categorie = 'private';
+    } else if(req.body.categorie === 'public') {
+      user.folder[catt_index].categorie = 'public';
     } else {
       return res.status(400).json({ message: 'Invalid categorie are send' });
     };
--- server/controllers/multer.js
+++ server/controllers/multer.js
@@ -1,15 +1,22 @@
 const express = require('express');
 const multer = require('multer');
-
+const fs = require('fs');
+const path = require('path');
 
 const storage = multer.diskStorage({
   destination: (req, file, callback) => {
+    let user_folder = JSON.parse(req.body.data_name);
+    let path_uploads = path.join(__dirname, '..', 'data', user_folder);
+    if(!fs.existsSync(path_uploads)) {  //? check if folder doesn't exist
+      fs.mkdirSync(path_uploads);
+    };
     callback(null, path_uploads);
   },
+
   filename: (req, file, callback) => {
     const data_name = file.originalname.split(' ').join('_');
     callback(null, Date.now() + '-' + data_name);
   }
 });
 
-module.exports =  multer({ storage: storage });
+module.exports = multer({ storage: storage });
