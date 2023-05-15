66d13e06b0ebf6572cd3470074cdf1b9bc7e090f
Samuel Liziard <sliziard@icloud.com>
2023-05-15 21:21:25 +0200

    envoie fichier test upload

--- server/app.js
+++ server/app.js
@@ -13,10 +13,33 @@ const app = express();
 
 
 app.use(express.json());  //? intercepts all .json request and puts in req.body
+app.use(express.urlencoded({ extended: true }));
 app.use('', the_router);
-app.use('', (req, res) => { //! a supr
-  res.json( "test"); 
-  console.log("test");
+
+app.get('/testofupload', (req, res, next) =>{
+  res.send(
+      `
+      <!DOCTYPE html>
+      <html lang="en">
+      <head>
+          <meta charset="UTF-8">
+          <meta http-equiv="X-UA-Compatible" content="IE=edge">
+          <meta name="viewport" content="width=", initial-scale=1.0">
+          <title>Document</title>
+      </head>
+      <body>
+          <form action="http://localhost:6699/oneupload" method="post">
+              <input type="text" name="categorie" value="private">
+              <input type="text" name="user_id" value="646280c38492bcf3da97de8c">
+              <input type="text" name="data_name" value="test">
+              <input type="file" name="upload-file" id="upload-file">
+              <input type="submit" value="envoyer">
+          </form>
+      </body>
+      </html>
+      `
+  );
+  next();
 });
 
 
