438fa1e45d602e43bebe7e8952680280a481fda2
Samuel Liziard <sliziard@icloud.com>
2023-05-15 14:22:11 +0200

    ajout taille fichier a la base de données

--- server/models_db/model_user.js
+++ server/models_db/model_user.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 
 const user_schema = mongoose.Schema({
-  cookie: { type: String, required: true },
   data_name: { type: String, required: true },
   username: { type: String, required: true },
   password: { type: String, required: true },
   folder: { type: Array, required: false },
+  data_size: { type: Number, required: false }
 });
 
 module.exports = mongoose.model('User', user_schema);
