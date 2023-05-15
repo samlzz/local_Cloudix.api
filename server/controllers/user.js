4e76cdbeda35e3664413733d4f32c88ddec4d080
Samuel Liziard <sliziard@icloud.com>
2023-05-15 14:21:44 +0200

    chiffrement mdp

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -9,33 +9,20 @@ exports.check_id_exist_and_passwrd_valid = (req, res) => {  //? check if the use
     let mdp_who_log = req.body.password;
     User.findOne({ data_name: req.body.data_name })
         .then(user_l => {
+            console.log(user_l._id)
             //? -->
             if (user_l === null) {  //? check if username are found
                 res.status(401).json({ message: 'Username not found'});
             } else {
-                //! quand mdp chiffré remplacer de là...
-                //? -->
-                if (mdp_who_log === user_l.password) {  //? check if passwords matches
-                    res.status(200).json( { 
-                        message: 'correct password',
-                        user_id: user_l._id, 
-                        cookie: user_l.cookie
-                    });
-                } else {
-                    res.status(401).json({ message: 'Wrong password'});
-                }
-                //! ... à là, par :
-                /*argon.verify(user_l.password, mdp_who_log)    //!mdp_who_log doit être non chiffré
+                argon.verify(user_l.password, mdp_who_log) //? check if passwords matches
                 .then(result => {
                     if (result) {
-                        res.status(200).json( {
-                            cookie: user_l.cookie
-                        });
+                        res.status(200).json({ user_id: user_l._id });
                     } else {
                         res.status(401).json({ message: 'Wrong password' });
                     }
                 })
-                .catch(error => res.status(500).json({ error }));*/
+                .catch(error => res.status(500).json({ error: error }));
             }
         })
         .catch(error => res.status(500).json({ error }));
@@ -48,17 +35,20 @@ exports.check_username_exist = (req, res) => {  //? check if the username don't 
     User.findOne({ data_name: usr_reg_min }).select({data_name: 1})
         .then(user_r => {
             if (user_r?.data_name === usr_reg_min) { 
-                res.status(400).json({ error: 'Username already exists' });
+                res.status(409).json({ error: 'Username already exists' });
             } else {  //? add new user
-                const user = new User({
-                    cookie: req.body.cookie, //TODO: frontend envoie cookie ? ou à générer
-                    data_name: usr_reg_min,
-                    username: user_registry,
-                    password: req.body.password //TODO: chiffré le mdp ? ou frontend le fait 
-                });
-                user.save()
-                .then(() => res.status(201).json({ message: 'User was created' }))
-                .catch(error => res.status(400).json({ error }));
+                argon.hash(req.body.password)
+                .then(hash_ofmdp =>{
+                    const user = new User({
+                        data_name: usr_reg_min,
+                        username: user_registry,
+                        password: hash_ofmdp 
+                    });
+                    user.save()
+                    .then(res.status(201).json({ message: 'User was created' }))
+                    .catch(error => res.status(400).json({ error }));
+                })
+                .catch(error => res.status(500).json(error));
             }
         })
         .catch(error => res.status(500).json({ error }));
