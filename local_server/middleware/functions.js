d1958c94af210549e82d36533557f03e72d51cd8
Samuel Liziard <sliziard@icloud.com>
2023-05-30 23:01:22 +0200

    empêche send reponse plusieurs fois

--- local_server/middleware/functions.js
+++ local_server/middleware/functions.js
@@ -1,20 +1,37 @@
 
-exports.returnSM = (res, code_status, retour, err = null)=>{
+exports.returnSM = (res, code_status, retour, err = null) => {
+    if(res.headersSent) {
+        console.log('A response was already sent');
+    } else {
         if(!err){
             return res.status(code_status).json({ status: code_status, message: retour });
         } else{
             return res.status(code_status).json({ status: code_status, message: retour, error: err });
         };
+    };
 };
 
-exports.returnFL = (res, code_status, file_list)=>{
-        return res.status(code_status).json({ status: code_status, files: file_list });
-};
-
-exports.check_and_return = (res, needto, code_status, retour)=>{
+exports.check_and_return = (res, needto, code_status, retour) => {
     if(!needto){
         return this.returnSM(res, code_status, retour);
     }else{
         return false;
     };
 };
+
+exports.returnFL = (res, code_status, file_list) => {
+    if(res.headersSent) {
+        console.log('A response was already sent');
+    } else {
+        return res.status(code_status).json({ status: code_status, files: file_list });
+    };
+};
+
+exports.returnID = (res, code_status, the_id, msg) => {
+    if(res.headersSent) {
+        console.log('A response was already sent');
+    } else {
+        return res.status(code_status).json({ status: code_status, user_id: the_id, message: msg });
+    };
+};
+
